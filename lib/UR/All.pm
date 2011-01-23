bfbe60a43b89fd52e4668f10cf862189f554fe66
Scott Smith <sakoht@cpan.org>
2011-01-23 21:45:44 +0000

    Import of SAKOHT/UR-0.28 from CPAN.
    gitpan-cpan-distribution: UR
    gitpan-cpan-version:      0.28
    gitpan-cpan-path:         SAKOHT/UR-0.28.tar.gz
    gitpan-cpan-author:       SAKOHT
    gitpan-cpan-maturity:     released

--- lib/UR/All.pm
+++ lib/UR/All.pm
@@ -3,7 +3,7 @@ package UR::All;
 use strict;
 use warnings;
 
-our $VERSION = "0.27"; # UR $VERSION;
+our $VERSION = "0.28"; # UR $VERSION;
 
 require above;
 use Class::Autouse; # 1.99_02, 1.99_04 or >= 2.0
@@ -96,6 +96,7 @@ use UR::ModuleConfig;
 use UR::ModuleLoader;
 use UR::Namespace;
 use UR::Namespace::Command;
+use UR::Namespace::Command::Base;
 use UR::Namespace::Command::Define;
 use UR::Namespace::Command::Define::Class;
 use UR::Namespace::Command::Define::Datasource;
